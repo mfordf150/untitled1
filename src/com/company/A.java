caecdde48a31b766ee8fa8f369aaf7535cc53164
Mikhail Ford <mfordf150@mail.ru>
2021-01-05 18:21:34 +0300

    TrainCompartment

--- src/com/company/A.java
+++ src/com/company/A.java
@@ -1,5 +1,14 @@
 package com.company;
-
+//В  вагоне 9 купе по 4 места в каждом. Сколько мест в вагоне?
 public class A {
+    public static void main(String[] args) {
+        int x = trainCompartment(9, 4);
+        System.out.println(x + " мест в вагоне ");
+    }
+
+    public static int trainCompartment(int z, int y) {
+      return z * y;
+    }
+
 
 }
